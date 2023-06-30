2a40daf620f117b22eab48e8c1f1d202589b3a17
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2023-06-30 20:55:57 +0530

    PrintNumberOfCharacterOccurence

--- PrintNumberOfCharacterOccurence.java
+++ PrintNumberOfCharacterOccurence.java
@@ -32,6 +32,21 @@ public class PrintNumberOfCharacterOccurence {
 		}
 		
 		System.out.println(numOfChar);
+		
+		Map<Character,Integer>numsOfChar1= new LinkedHashMap<Character,Integer>();
+		
+		for(int i =0 ;i<valueArray.length;i++)
+		{
+			numsOfChar1.put(valueArray[i],numsOfChar1.getOrDefault(valueArray[i],0)+1);
+		}
+		System.out.println(numsOfChar1);
 	}
+	
+	//another way
+	
+	
+	
+			
+			
 
 }
